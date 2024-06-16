ae9ccc79856bb928752bc68d48c04e9dc3ec32f5
smelik81 <artem.smelyanskiy81@gmail.com>
2024-06-16 21:05:43 +0300

    update components Feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,11 @@
-function Feedback({ good, bad, neutral, totalFeedback, updateReset }) {
+function Feedback({ good, bad, neutral, totalFeedback, positiveFeedback }) {
   return (
     <div>
       <p>Good: {good}</p>
       <p>Bad: {bad}</p>
       <p>Neutral: {neutral}</p>
       <p>Total: {totalFeedback}</p>
-      <p>Positive: {updateReset}</p>
+      <p>Positive: {`${positiveFeedback}%`}</p>
     </div>
   );
 }
