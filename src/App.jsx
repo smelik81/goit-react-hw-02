1e9478965ba0ecbb237d3c1d2159fc6092f8fa0d
smelik81 <artem.smelyanskiy81@gmail.com>
2024-06-16 17:42:56 +0300

    started task-2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* import { useState } from 'react'; */
+import './App.css';
+
+function App() {
+  const feedBackState = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  return <></>;
+}
+
+export default App;
